2f451c0ffa373f08687aef795379dafa9f4b24a9
S . M. Shakib Limon <shakibcse3842@gmail.com>
2018-07-20 01:20:46 +0600

    creating list view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+
+from users import models, serializers
+
+
+class UserListView(generics.ListCreateAPIView):
+    queryset = models.CustomUser.objects.all()
+    serializer_class =  serializers.UserSerializer
+
