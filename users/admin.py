2468776d17858d5afbc7458dfe100cac49fe88bb
S . M. Shakib Limon <shakibcse3842@gmail.com>
2018-07-20 00:49:09 +0600

    User admin updated

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
+from users.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    model = CustomUser
+
+    list_display = ['email' , 'username' , 'name']
+
+admin.site.register(CustomUser ,CustomUserAdmin)
+
